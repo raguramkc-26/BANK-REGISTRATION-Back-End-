bacbfb1d4779f54e66b6bb17517ef6ac02e6cd20
raguramkc-26 <raguramkc038@gmail.com>
2026-06-08 20:59:31 +0530

    changes done

--- server.js
+++ server.js
@@ -2,45 +2,28 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
-const connectDB =
-require("./config/db");
-
 dotenv.config();
 
+const connectDB = require("./config/db");
 connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
-app.use(
-  "/api/users",
-  require("./routes/userRoutes")
-);
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/atm", require("./routes/atmRoutes"));
+app.use("/api/loan", require("./routes/loanRoutes"));
 
-app.use(
-  "/api/atm",
-  require("./routes/atmRoutes")
-);
-
-app.use(
-  "/api/loans",
-  require("./routes/loanRoutes")
-);
-
-app.get("/", (req, res) => {
-  res.send(
-    "Unity Bank Backend Running"
-  );
-});
-
-const PORT =
-process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(
-    `Server running on ${PORT}`
-  );
+  console.log(`Server running on port ${PORT}`);
 });
