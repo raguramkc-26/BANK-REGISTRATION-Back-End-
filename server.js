d2c7eb416d3a1a0e5fc98e0e498af30410141041
raguramkc-26 <raguramkc038@gmail.com>
2026-06-08 23:09:49 +0530

    changes done

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+    "http://localhost:5173",
+    "https://bank-registration-red.vercel.app"
+    ],
     credentials: true,
   })
 );
